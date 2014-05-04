5304296ac00dd4fbe6f7c881b1a2723b9768cc01
mathur7 <mathur7@gmail.com>
2014-05-04 12:16:57 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FrugalEater::Application.config.secret_key_base = '3e7f8043852b9056e97da9e6e18d305c840d6fcd3d115359dccb71e189ef56b33cd17dda614514feac4ba64513ee1c838b970816b1102e347c5e37fba64f4cab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FrugalEater::Application.config.session_store :cookie_store, key: '_FrugalEater_session'
