befd3437d3d46f74fb20e87c0789633103bdfbc0
mathur7 <mathur7@gmail.com>
2014-05-04 12:41:20 -0700

    installed devise initializer and model

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,8 @@ FrugalEater::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.force_ssl = true
+
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
