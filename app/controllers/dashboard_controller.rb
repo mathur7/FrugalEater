0d04d5441a5852d04748c982b71d4c6b5cfda168
mathur7 <mathur7@gmail.com>
2014-05-04 22:39:03 -0700

    added methods to 3 controllers, favorites controller is empty still

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
 before_action :authenticate_user!
 
   def index
-    
+    @menuitem = Menuitem.new
   end
 
 end
--- app/controllers/menuitems_controller.rb
+++ app/controllers/menuitems_controller.rb
@@ -4,4 +4,38 @@ class MenuitemsController < ApplicationController
     @menuitem = Menuitem.new
   end
 
+  # def show
+  #   @menuitem = Menuitem.find(params[:id])
+  # end
+
+  # def new
+  #   @menuitems = Menuitem.all
+  # end
+
+  # def create
+  #   menuitem = Menuitem.create menuitem_params
+  #   redirect_to(menuitem)
+  # end
+
+  # def edit
+  #   @menuitem = Menuitem.find(params[:id])
+  # end
+
+  # def update
+  #   @menuitem = Menuitem.find(params[:id])
+  #   @menuitem.update menuitem_params
+  #   redirect_to menuitem_path(@menuitem.id)
+  # end
+
+  # def destroy
+  #   menuitem.find(params[:id]).destroy
+  #   redirect_to root_path
+  # end
+
+  # private
+  # def menuitem_params
+  #   params.require(:menuitem).permit(:restaurant, :name, :price)
+  # end
+
+  
 end
