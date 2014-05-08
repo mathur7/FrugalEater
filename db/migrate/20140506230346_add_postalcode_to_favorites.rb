595a20ffac8073c7918f01b8351d64f57a6a0d3a
mathur7 <mathur7@gmail.com>
2014-05-07 19:13:32 -0700

    added a locu_id column to menuitems table, deleted some unnecessary columns

--- db/migrate/20140506230346_add_postalcode_to_favorites.rb
+++ db/migrate/20140506230346_add_postalcode_to_favorites.rb
@@ -1,7 +1,5 @@
 class AddPostalcodeToFavorites < ActiveRecord::Migration
   def change
-    add_column :favorites, :AddLocalityToFavorites, :string
-    add_column :favorites, :AddRegionToFavorites, :string
     add_column :favorites, :postal_code, :integer
     add_column :favorites, :locality, :text
     add_column :favorites, :region, :text
