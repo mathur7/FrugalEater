58eb1074ee9eb44977a7a584cc5311c5e4a3a9a1
mathur7 <mathur7@gmail.com>
2014-05-10 09:09:34 -0700

    testing for errors in controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -19,6 +19,12 @@ class FavoritesController < ApplicationController
     redirect_to favorites_path
   end
 
+  def update
+    @favorite = favorite.find(params[:id])
+    @favorite.update favorite_params
+    redirect_to favorites(@favorite.id)
+  end
+
   private
   def favorite_params
       params.require(:favorite).permit(:name, :description, :price, :restaurant, :address, :postal_code, :locality, :region, :user_id)
